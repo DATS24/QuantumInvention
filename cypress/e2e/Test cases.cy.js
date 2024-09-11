c0bbc290ef1663e1464b170f73272ad0a2ba9c4b
RivaldiS05 <revaldi.s05@gmail.com>
2024-09-11 08:56:32 +0700

    create

--- cypress/e2e/Test cases.cy.js
+++ cypress/e2e/Test cases.cy.js
@@ -1,32 +1,101 @@
 
 describe('Test cases', () => {
-  before(() => {
-    cy.visit('https://www.traveloka.com/')
+  beforeEach(() => {
+    cy.visit('https://www.onemap.gov.sg/')
   })
 
-  it('All Test Cases', () => {
-    //1
-    let Keyword = 'Bandung'
-    cy.get('[data-testid="product-pill-Hotels"]').click({force:true})
-    cy.get('[placeholder="City, hotel, place to go"]').should('be.visible').click({force:true}).type(Keyword+'{enter}')
-    cy.get('[data-testid="dropdown-menu-item"]').contains('span', Keyword).click({force:true})
-    //2
-    cy.get('[data-testid="check-in-date-field"]').click({force:true})
-    cy.get('[class="css-1dbjc4n r-1awozwy r-kdyh1x r-1loqt21 r-1777fci r-1otgn73 r-1i6wzkk r-lrvibr"]').then(($list) => {
-      cy.get($list).eq(7).click({force:true})
-      //3
-      cy.get($list).eq(8).click({force:true})
+  afterEach(() => {
+    cy.window().then((win) => {
+      cy.stub(win, 'close').as('close')
     })
-    //4
-    cy.get('[data-testid="occupancy-field"]').click({force:true})
-    cy.get('[data-testid="occupancy-adult"]').within(() => {
-      cy.get('[stroke="#0194f3"]').click({force:true})
-    })
-    //5
-    cy.get('[data-testid="search-submit-button"]').click({force:true})
-    //6
-    cy.get('[data-testid="tvat-hotelLocation"').each(($el) => {
-      cy.wrap($el).should("contain.text", "Bandung")
-    })
+  })
+
+  it ('Search a place on map', () => {
+    let Keyword = 'CHANGI'
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains(Keyword).click()
+    cy.get('.markerInfoboxwrapper').should('contain', Keyword)
+  })
+
+  it ('Search a nearby', () => {
+    let Keyword = 'Nearby '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_address').contains('Search '+Keyword).click()
+    cy.get('#markerInfoboxwrapper').click({force:true}).should('exist')
+  })
+
+  it ('Search school by school query map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('School Query').click()
+    cy.get('#secSchool').click()
+    cy.get('.schoolQueryDisclaimerBlockBtn').click()
+    cy.get('.schoolListItem').first().click().invoke('text').as('Result')
+    const Result = this.Result
+    cy.get('#markerInfoContent').should('contain.text', Result)
+  })
+
+  it ('Search land lot number by map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('Land Query').click()
+    cy.get('.landinfo-tenure').click()
+    cy.get('.landInfoTenureDisclaimerBlockBtn').click()
+    cy.get('#tenure-address-strata').click()
+    cy.get('#prefix').type('26')
+    cy.get('#lot-no').type('08092')
+    cy.get('#suffix').type('A')
+    cy.get('.btn-big.tenure-search').click({force:true})
+    cy.get('.cl-body').should('exist')
+  })
+
+  it ('Search drone zone by map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('Drone Query').click()
+    cy.get('.dronebtnAccept').click()
+  })
+
+  it ('Search nearby by nearby info map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('Nearby Info').click()
+    cy.get('#Amenities').click()
+    cy.get('#axs_station').click()
+    cy.get('#markerInfoTitle').should('exist')
+  })
+  
+  it ('Search nearby by nearby info map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('Nearby Info').click()
+    cy.get('#Amenities').click()
+    cy.get('#axs_station').click()
+    cy.get('#markerInfoTitle').should('exist')
+  })
+
+  it ('Choose map style by map apps', () => {
+    let Keyword = 'Map Apps '
+    cy.get('.search-location-input').type(Keyword)
+    cy.wait(5000)
+    cy.get('.searchresult_name').contains('Map Styles').click()
+    cy.get('[basemapid="Original"]').click()
+  })
+
+  it ('Search a route', () => {
+    let origin = 'changi'
+    let destination = 'welfare'
+    cy.get('#originInput').type(origin+{enter})
+    cy.get('#destLocation').type(destination+{enter})
+    cy.wait(5000)
+    cy.get('#routeOptionItem_Car').click()
+    cy.get('#itineraryItem').should('be.visible')
   })
 })
